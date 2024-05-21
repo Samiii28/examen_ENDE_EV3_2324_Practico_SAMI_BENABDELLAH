ed5b487d70c57072ef3cd0fa245f2c65095f13c5
sami <samitrabajos@gmail.com>
2024-05-21 13:38:19 +0200

    Validacion inicial

---
+++ src/examen_ENDE_EV3_Practico_SAMI_BENABDELLAH/ClaseRamaMaster.java
@@ -0,0 +1,8 @@
+package examen_ENDE_EV3_Practico_SAMI_BENABDELLAH;
+
+public class ClaseRamaMaster {
+
+	public static void main (String[] args) {
+		System.out.println("Primer fcihero del paquete git en la rama Master");
+	}
+}
